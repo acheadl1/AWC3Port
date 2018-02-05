9735438b97b85b6dceffeabd5d2cb1bae1bfd5d2
acheadl1 <32757977+acheadl1@users.noreply.github.com>
2018-02-05 01:41:23 -0500

    Pre-Content S1
    -Skillbars, footer, functionality additions

--- js/action.js
+++ js/action.js
@@ -1,3 +1,5 @@
+// Stick Header [START]
+
 $(document).ready(function(){
 
   $(window).resize(function(){
@@ -19,5 +21,133 @@ $(document).ready(function(){
     });
 
   }).resize();
-  
+
+//Sticky Header [END]
+
+//User Skill Lines [START]
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line1").removeClass("skill-less");
+    } else {
+        $("#skill-line1").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line2").removeClass("skill-less");
+    } else {
+        $("#skill-line2").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line3").removeClass("skill-less");
+    } else {
+        $("#skill-line3").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line4").removeClass("skill-less");
+    } else {
+        $("#skill-line4").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line5").removeClass("skill-less");
+    } else {
+        $("#skill-line5").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line6").removeClass("skill-less");
+    } else {
+        $("#skill-line6").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line7").removeClass("skill-less");
+    } else {
+        $("#skill-line7").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line8").removeClass("skill-less");
+    } else {
+        $("#skill-line8").addClass("skill-less");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var aboutSection = $("#about");
+    var aboutPosition = aboutSection.offset().top;
+    if (scroll >= aboutPosition) {
+        $("#skill-line9").removeClass("skill-less");
+    } else {
+        $("#skill-line9").addClass("skill-less");
+    }
+});
+
+//LinK Smooth Scrolling [START]
+
+$("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 850, function(){
+   
+          window.location.hash = hash;
+      });
+    }
+
+
+  });
+
+//LinK Smooth Scrolling [END]
+
+
+
 });
