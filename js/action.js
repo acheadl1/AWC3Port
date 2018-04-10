5f0846043f870c782d4be0d80ea6a883a2cef88b
acheadl1 <32757977+acheadl1@users.noreply.github.com>
2018-04-10 13:56:08 -0400

    Ready for Launch S1
    -Added in ghost headers & ghost fade in
    -Updated all content for modals including descriptions and thumbnails
    -Mobile styling!
    --No skillbar for mobile, reverted modal overrides, added mobile only footer

--- js/action.js
+++ js/action.js
@@ -211,6 +211,21 @@ $(window).scroll( function(){
     
     });
 
+$(window).scroll( function(){
+    
+        $('.ghost-in').each( function(i){
+            
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){
 
+                $(this).animate({'opacity':'.2'},500);
+                    
+            }
+            
+        }); 
+    
+    });
 
 });
